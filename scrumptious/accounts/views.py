1487705cac9c74810e151aabff1419eca9216c46
Yadriel <147550067+Yadriel-Ruiz@users.noreply.github.com>
2024-01-25 17:50:37 -0600

    created login page

--- scrumptious/accounts/views.py
+++ scrumptious/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from accounts.forms import SignUpForm, LoginForm
 
